a7a5ba6790d3833bbc5b91ece9e901172231b22f
John O'Keefe <jokeefe@fastmail.com>
2024-02-19 18:52:42 -0500

    added ability to count letters in book

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ def main():
     text = get_book_text(book_path)
     count_of_words = count_words_in_book(text)
     print(f"{count_of_words} words found in the document")
+    count_of_letters = count_letters(text)
+    print(count_of_letters)
 
 
 def get_book_text(path):
@@ -11,5 +13,16 @@ def get_book_text(path):
     
 def count_words_in_book(book):
     return len(book.split())
+
+def count_letters(book):
+    letter_dict = {}
+    book_list = list(book)
+    for letter in book_list:
+        lowercase_letter = letter.lower()
+        if lowercase_letter not in letter_dict.keys():
+            letter_dict[lowercase_letter] = 1
+        else:
+            letter_dict[lowercase_letter] += 1
+    return letter_dict
 
 main()
