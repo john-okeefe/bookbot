641c7d08be42de540248c9a81d44313faafbf29e
John O'Keefe <jokeefe@fastmail.com>
2024-02-19 20:12:33 -0500

    finished print report

--- main.py
+++ main.py
@@ -2,9 +2,14 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     count_of_words = count_words_in_book(text)
-    print(f"{count_of_words} words found in the document")
     count_of_letters = count_letters(text)
-    print(count_of_letters)
+    human_readable_letter_count = map(human_readable_letter_count_map, dict_sort_and_convert(count_of_letters))
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{count_of_words} words found in the document")
+    print("\n")
+    for sentence in list(human_readable_letter_count):
+        print(sentence)
+    print("--- End report ---")
 
 
 def get_book_text(path):
@@ -16,13 +21,27 @@ def count_words_in_book(book):
 
 def count_letters(book):
     letter_dict = {}
-    book_list = list(book)
-    for letter in book_list:
+    for letter in book:
         lowercase_letter = letter.lower()
-        if lowercase_letter not in letter_dict.keys():
-            letter_dict[lowercase_letter] = 1
-        else:
-            letter_dict[lowercase_letter] += 1
+        if lowercase_letter.isalpha() == True:
+            if lowercase_letter not in letter_dict.keys():
+                letter_dict[lowercase_letter] = 1
+            else:
+                letter_dict[lowercase_letter] += 1
     return letter_dict
+
+def sort_on(dict):
+    return dict["count"]
+
+def dict_sort_and_convert(dict):
+    dict_list = []
+    for key in dict.keys():
+        new_dict = {"letter": key, "count": dict[key]}
+        dict_list.append(new_dict)
+    dict_list.sort(reverse=True, key=sort_on)
+    return dict_list
+
+def human_readable_letter_count_map(dict):
+    return f"The '{dict["letter"]}' character was found {dict["count"]} times"
 
 main()
